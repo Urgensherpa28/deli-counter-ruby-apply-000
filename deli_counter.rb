a09f7731ec86dd04aab3ca151948f6435a088ee8
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-19 23:11:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(katz_deli)
+  katz_deli = []
+  puts "The line is currently empty."
+
+  line(other_deli)
+end
+
+def take_a_number(line, name )
+  puts "The line is currently: #{line(katz_deli).index}"
+end
